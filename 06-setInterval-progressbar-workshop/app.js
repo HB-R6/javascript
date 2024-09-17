819bda4592ae9d7ebbf567e23ef088fe87e2cd2a
Lucas Delobelle <lucas@ld-web.net>
2024-09-17 14:39:11 +0200

    :sparkles: Progress bar workshop

--- 06-setInterval-progressbar-workshop/app.js
+++ 06-setInterval-progressbar-workshop/app.js
@@ -1,15 +1,27 @@
 let progress = 0;
 const step = 10;
+let intervalId; // undefined
 
 document.addEventListener("DOMContentLoaded", () => {
   const uploadBtn = document.querySelector("#uploadBtn");
   const progressBar = document.querySelector(".progressBar .inner");
+  const svgContainer = document.querySelector("#confirm");
 
   uploadBtn.addEventListener("click", () => {
-    setInterval(() => {
+    progress = 0;
+    svgContainer.style.display = "none";
+    uploadBtn.disabled = true;
+
+    intervalId = setInterval(() => {
       progress += step;
       progressBar.style.width = `${progress}%`;
       progressBar.innerText = `${progress}%`;
-    }, 500);
+
+      if (progress >= 100) {
+        clearInterval(intervalId);
+        svgContainer.style.display = "block";
+        uploadBtn.disabled = false;
+      }
+    }, 400);
   });
 });
