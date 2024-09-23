c5bf864bfbeb5c472e24c0db0d587939203008c2
Lucas Delobelle <lucas@ld-web.net>
2024-09-23 11:48:58 +0200

    :sparkles: Login, articles list WIP

--- 10-jwt/index.js
+++ 10-jwt/index.js
@@ -5,3 +5,20 @@ const jwt = localStorage.getItem(tokenName);
 if (jwt === null) {
   window.location.href = "login.html";
 }
+
+const articlesContainer = document.querySelector("#articles-container");
+
+(async () => {
+  const res = await fetch("http://localhost:8000/api/articles", {
+    headers: {
+      Authorization: `Bearer ${jwt}`,
+    },
+  });
+  const data = await res.json();
+
+  data["hydra:member"].map((article) => {
+    const li = document.createElement("li");
+    li.innerText = article.title;
+    articlesContainer.appendChild(li);
+  });
+})(); // IIFE
--- 10-jwt/login.js
+++ 10-jwt/login.js
@@ -4,7 +4,7 @@ const loginForm = document.querySelector("#login-form");
 // alors on installe un écouteur (une fonction)
 // qui sera déclenchée lors de cet événement.
 // Ainsi, lorsque l'écouteur est déclenché, il reçoit en paramètre l'événement en question
-loginForm.addEventListener("submit", (event) => {
+loginForm.addEventListener("submit", async (event) => {
   event.preventDefault();
 
   // Je construis un objet FormData à partir du formulaire existant
@@ -13,16 +13,26 @@ loginForm.addEventListener("submit", (event) => {
   const username = formData.get("email");
   const password = formData.get("password");
 
-  fetch("http://localhost:8000/api/login_check", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    // Dans le body, je lui demande de transformer un objet en string
-    // L'objet en question portera une propriété "username" avec la valeur de username définie plus haut, et idem avec "password"
-    body: JSON.stringify({ username, password }),
-  })
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-    .catch((err) => console.error("Une erreur est survenue : ", err));
+  try {
+    const res = await fetch("http://localhost:8000/api/login_check", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // Dans le body, je lui demande de transformer un objet en string
+      // L'objet en question portera une propriété "username" avec la valeur de username définie plus haut, et idem avec "password"
+      body: JSON.stringify({ username, password }),
+    });
+
+    if (!res.ok) {
+      console.error("Erreur survenue lors de l'authentification");
+      return;
+    }
+
+    const data = await res.json();
+    localStorage.setItem("JWT_TOKEN", data.token);
+    window.location.href = "index.html";
+  } catch (err) {
+    console.error("Erreur CATCH : ", err);
+  }
 });
